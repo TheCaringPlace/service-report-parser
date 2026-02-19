23d7e00705427222ab9989c374f76d2856e06a74
Dan Klco <klco@adobe.com>
2026-02-19 09:04:07 -0500

    Adding CSV export

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 import { program } from "commander";
+import { createWriteStream } from "fs";
 import { readFile } from "fs/promises";
 import { join, normalize } from "path";
+import fastcsv from "fast-csv";
+import {flatten}  from "flat";
 import { crawlDirectory, writeFileWithMkdir } from "./lib/fs.js";
 import { extractTextFromPdf } from "./lib/pdf.js";
 import { syncFilesFromS3 } from "./lib/sync-s3.js";
@@ -51,7 +54,10 @@ program
       }
       console.info(`Extracting text from ${file}`);
       const text = await extractTextFromPdf(file);
-      const targetPath = join(output, file.replace(input + "/", "").replace(".pdf", ".txt"));
+      const targetPath = join(
+        output,
+        file.replace(input + "/", "").replace(".pdf", ".txt"),
+      );
       console.info(`Saving text to ${targetPath}`);
       await writeFileWithMkdir(targetPath, text.text);
     }
@@ -79,11 +85,16 @@ program
       const result = await extractTextFromPdf(file);
 
       const report = parseReport(result);
-      if(report.reportType == null) {
-        console.warn(`Skipping report ${file} because it could not be identified`);
+      if (report.reportType == null) {
+        console.warn(
+          `Skipping report ${file} because it could not be identified`,
+        );
         continue;
       }
-      const targetPath = join(output, normalize(file).replace(normalize(input), "").replace(".pdf", ".json"));
+      const targetPath = join(
+        output,
+        normalize(file).replace(normalize(input), "").replace(".pdf", ".json"),
+      );
       await writeFileWithMkdir(targetPath, JSON.stringify(report, null, 2));
     }
   });
@@ -96,17 +107,32 @@ program
     "The folder containing the report JSON files",
   )
   .requiredOption(
-    "-o, --output <output-file>",   
-    "The file to save the consolidated JSON to",
+    "-o, --output <output-folder>",
+    "The file to save the consolidated JSON and CSV",
   )
   .action(async ({ input, output }) => {
     const files = await crawlDirectory(input);
-    const reports = await Promise.all(files.map(async (file) => {
-      const text = await readFile(file, "utf8");
-      return JSON.parse(text);
-    }));
+    const reports = await Promise.all(
+      files.map(async (file) => {
+        const text = await readFile(file, "utf8");
+        return JSON.parse(text);
+      }),
+    );
     const consolidatedReports = consolidateReports(reports);
-    await writeFileWithMkdir(output, JSON.stringify(consolidatedReports, null, 2));
+    await writeFileWithMkdir(
+      join(output, "consolidated.json"),
+      JSON.stringify(consolidatedReports, null, 2),
+    );
+
+    const csvFile = join(output, "consolidated.csv");
+    const ws = createWriteStream(csvFile);
+
+    fastcsv
+      .write(consolidatedReports.map(report => flatten(report)), { headers: true })
+      .pipe(ws)
+      .on("finish", () => {
+        console.log(`Finished writing data to: ${csvFile}`);
+      });
   });
 
 program.parse(process.argv);
